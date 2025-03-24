f099a1961b31d03fdea6e9bca2dc91f7ef1aec9b
aom214 <aom.k@ahduni.edu.in>
2025-03-24 15:20:07 -0700

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -13,13 +13,9 @@ const startserver = async () => {
     console.log("Database connected");
 
     const port = process.env.PORT || 3000;
-    console.log(`PORT from env: ${process.env.PORT}`);
-console.log(`Final PORT: ${port}`);
-
-app.listen(port, "0.0.0.0", () => {  // ✅ Bind to 0.0.0.0
-    console.log(`Server running on port:- http://localhost:${port}`);
-});
-
+    app.listen(port, () => {
+      console.log(`Server running on port:- http://localhost:${port}`);
+    });
   } catch (error) {
     console.log(error.message);
     process.exit(1);
