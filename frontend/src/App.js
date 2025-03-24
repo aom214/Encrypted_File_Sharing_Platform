e4929c4724cfd0941ff73c2ca57eda16cce9ea88
aom214 <aom.k@ahduni.edu.in>
2025-03-24 16:47:49 -0700

    mcidnv

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,16 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import { setUser, logoutUser } from "./redux/authSlice.js"; 
+import { setUser, logoutUser } from "./redux/authSlice.js"; // Import actions
 import Login from "./components/Login.js";
 import Dashboard from "./components/Dashboard.js";
-import Files from "./components/Files.js"; 
+import Files from "./components/Files.js"; // Import the Files component
 import PrivateRoute from "./components/PrivateRoute.js";
 import Signup from "./components/Signup.js";
-import RequestsPage from "./components/Notifications.js"; 
+import RequestsPage from "./components/Notifications.js"; // ✅ Import RequestsPage component
 import AddFriendsPage from "./components/Addfrined.js";
-import Home from "./components/Home.js";
+import Home from "./components/Home.js"
 import UserProfile from "./components/userprofile.js";
-import { HashRouter as Router } from "react-router-dom";
-
 
 const App = () => {
   const dispatch = useDispatch();
@@ -53,18 +51,19 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={user ? <PrivateRoute><Home onLogout={handleLogout} /></PrivateRoute> : redirectToLogin()} />
-        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
-        <Route path="/signup" element={user ? <Navigate to="/" /> : <Signup />} />
-        <Route path="/profile" element={user ? <PrivateRoute><UserProfile onLogout={handleLogout} /></PrivateRoute> : redirectToLogin()} />
-        <Route path="/share-file" element={user ? <PrivateRoute><Dashboard onLogout={handleLogout} /></PrivateRoute> : redirectToLogin()} />
-        <Route path="/files" element={user ? <PrivateRoute><Files onLogout={handleLogout} /></PrivateRoute> : redirectToLogin()} />
-        <Route path="/request" element={user ? <PrivateRoute><RequestsPage onLogout={handleLogout}/></PrivateRoute> : redirectToLogin()} />
-        <Route path="/add-friends" element={user ? <PrivateRoute><AddFriendsPage onLogout={handleLogout}/></PrivateRoute> : redirectToLogin()} />
-        
-        {/* ✅ Catch-all route (Redirects to Home if no match is found) */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+  <Route path="/" element={user ? <PrivateRoute><Home onLogout={handleLogout} /></PrivateRoute> : redirectToLogin()} />
+  <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+  <Route path="/signup" element={user ? <Navigate to="/" /> : <Signup />} />
+  <Route path="/profile" element={user ? <PrivateRoute><UserProfile onLogout={handleLogout} /></PrivateRoute> : redirectToLogin()} />
+  <Route path="/share-file" element={user ? <PrivateRoute><Dashboard onLogout={handleLogout} /></PrivateRoute> : redirectToLogin()} />
+  <Route path="/files" element={user ? <PrivateRoute><Files onLogout={handleLogout} /></PrivateRoute> : redirectToLogin()} />
+  <Route path="/request" element={user ? <PrivateRoute><RequestsPage onLogout={handleLogout}/></PrivateRoute> : redirectToLogin()} />
+  <Route path="/add-friends" element={user ? <PrivateRoute><AddFriendsPage onLogout={handleLogout}/></PrivateRoute> : redirectToLogin()} />
+
+  {/* ✅ Catch-all route (Redirects to Home if no match is found) */}
+  <Route path="*" element={<Navigate to="/" />} />
+</Routes>
+
     </Router>
   );
 };
